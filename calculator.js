0ed469e70e8052f9bb95c0965b87701a283f25eb
avi212003 <chauhanavi843@gmail.com>
2024-09-15 18:51:47 -0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,15 +1,21 @@
-// Function to divide two numbers by Avi and shravani
+// Example usage
+// Function to add two numbers
+function add(a, b) {
+   return a + b;
+}
 
+// Function to subtract two numbers
+function subtract(a, b) {
+   return a - b;
+}
+
+//Jainik and Harshini
 // Function to multiply two numbers
 function multiply(a, b) {
   return a * b;
 }
 
-//Jainik and Harshini
-// Function to subtract two numbers
-function subtract(a, b) {
-  return a - b;
-// Function to divide two numbers
+// Function to divide two numbers by Avi and shravani
 function divide(a, b) {
    if (b === 0) {
    return "Error: Division by zero";
@@ -17,21 +23,12 @@ function divide(a, b) {
    return a / b;
 }
 
-// Example usage
-// Function to add two numbers
-function add(a, b) {
-   return a + b;
-}
-
 // Example by dileep
 const a = 10;
 const b = 5;
 
 // Display results on console
-console.log(`Multiplication: ${a} * ${b} = ${multiply(a, b)}`);
-
-console.log(`Subtraction: ${a} - ${b} = ${subtract(a, b)}`);
 console.log(`Addition: ${a} + ${b} = ${add(a, b)}`);
-
-
+console.log(`Subtraction: ${a} - ${b} = ${subtract(a, b)}`);
+console.log(`Multiplication: ${a} * ${b} = ${multiply(a, b)}`);
 console.log(`Division: ${a} / ${b} = ${divide(a, b)}`);
